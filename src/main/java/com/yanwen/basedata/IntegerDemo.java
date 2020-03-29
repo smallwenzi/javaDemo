c7f1a6c85c9ab7a5ae1c8a2257a8fad039b8e106
tanwen tan <beiyanwen@gmail.com>
2020-03-29 14:26:49 +0800

    Integer最大值加2 会溢出

---
+++ src/main/java/com/yanwen/basedata/IntegerDemo.java
@@ -0,0 +1,9 @@
+package com.yanwen.basedata;
+
+public class IntegerDemo {
+    public static void main(String args[]) {
+        int num = 2147483647 ;
+        num += 2L ;
+        System.out.println(num) ;
+    }
+}
