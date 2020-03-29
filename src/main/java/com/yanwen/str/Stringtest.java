1d5e736b269064b640c3271bde51a800019534ab
tanwen tan <beiyanwen@gmail.com>
2020-03-29 14:29:57 +0800

    Object 的 equals 方法容易抛空指针异常，应使用常量或确定有值的对象来调用 equals。 正例："test".equals(object); 反例：object.equals("test"); 说明：推荐使用 java.util.Objects#equals（JDK7 引入的工具类）。

---
+++ src/main/java/com/yanwen/str/Stringtest.java
@@ -0,0 +1,8 @@
+package com.yanwen.str;
+
+public class Stringtest {
+  public static void  main(String[] args){
+     System.out.print("test".equals(null));
+      System.out.print(null instanceof  String);
+    }
+}
